5b29c4f3872ff55f2644dd4dd0a21b601536864a
Arnaud Levy <contact@arnaudlevy.com>
2026-06-01 21:29:27 +0200

    update

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source "https://rubygems.org"
 
-ruby "3.2.2"
+ruby "3.4.9"
 
-gem "rails", "~> 7.1.2"
+gem "rails", "~> 7.2.3.1"
 gem "sprockets-rails"
 gem "pg", "~> 1.1"
 gem "puma", ">= 5.0"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_21_112625) do
+ActiveRecord::Schema[7.2].define(version: 2023_12_21_112625) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,5 +31,4 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_21_112625) do
     t.string "diplom_path"
     t.date "diplom_date"
   end
-
 end
